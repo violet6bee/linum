3587101317cf5039b44f77f42658e9c41e9e2e32
violet6bee <deadviolet@yandex.ru>
2026-04-04 15:26:18 +0300

    добавлены метод для синхронизации версии пакета в репозитории и метод для сравнения пакетов и репозиториев

--- linum-server/src/main/java/ru/shvalieva/repository/RepoPackageRepository.java
+++ linum-server/src/main/java/ru/shvalieva/repository/RepoPackageRepository.java
@@ -1,8 +1,15 @@
 package ru.shvalieva.repository;
 
+import ru.shvalieva.model.PackageEntity;
 import ru.shvalieva.model.RepoPackage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.shvalieva.model.Repository;
+
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RepoPackageRepository extends JpaRepository<RepoPackage, UUID> {
+    Optional<RepoPackage> findByRepositoryAndPackageEntity(Repository repository, PackageEntity packageEntity);
+    List<RepoPackage> findAllByPackageEntity(PackageEntity packageEntity);
 }
